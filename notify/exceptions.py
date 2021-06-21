f912d1d36b1da06baf96ead901388ad2e2f7f07c
Melvin Folkers <melvin@yellowstacks.nl>
2021-06-21 15:15:12 +0200

    added exception if dataframe is larger than 30 records

--- notify/exceptions.py
+++ notify/exceptions.py
@@ -2,3 +2,9 @@ class EnvironmentVariablesError(Exception):
     """Exception when not all env variables are set"""
 
     pass
+
+
+class DataFrameTooLarge(Exception):
+    """Exception when the dataframe is too large to be readable in the notification"""
+
+    pass
