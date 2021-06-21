94fee1ef697edf4214ba15d89164b577e961e8e8
Melvin Folkers <melvin@yellowstacks.nl>
2021-06-21 15:15:44 +0200

    moved test dataframes to init

---
+++ notify/tests/__init__.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+
+def import_sample_dfs(transactions: int = 7):
+    """
+
+    Parameters
+    ----------
+    transactions: int
+        number of transactions to generate.
+
+    Returns
+    -------
+
+    """
+    df1 = pd.DataFrame({"numbers": [1, 2, 3], "colors": ["red", "white", "blue"]})
+
+    transactions = transactions
+
+    df2 = pd.DataFrame(index=range(transactions))
+    df2["amount"] = [np.random.randint(1000, 10000) for x in range(transactions)]
+    df2["date"] = [
+        (pd.to_datetime("now") - pd.Timedelta(value=x, unit="days")).strftime("%Y-%m-%d")
+        for x in range(transactions)
+    ]
+
+    dfs = {"Metadata": df1, "Transactions": df2}
+
+    return dfs
