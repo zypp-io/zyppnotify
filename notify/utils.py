e32150b3befbe1765ab5b435c801d63914cab7c3
Melvin Folkers <melvin@yellowstacks.nl>
2021-06-21 15:16:03 +0200

    added html formatting of table to utils

--- notify/utils.py
+++ notify/utils.py
@@ -1,4 +1,7 @@
 import os
+
+import pandas as pd
+
 from notify.exceptions import EnvironmentVariablesError
 
 
@@ -21,3 +24,77 @@ def check_environment_variables(required_variables: list):
         raise EnvironmentVariablesError(
             f"One of the environment variables {', '.join(required_variables)} is not set"
         )
+
+
+def dataframe_to_html(df: pd.DataFrame) -> str:
+    """
+
+    Parameters
+    ----------
+    df
+
+    Returns
+    -------
+
+    """
+
+    html_table = df.to_html(index=False, classes="styled-table", justify="center")
+
+    pretty_html_table = (
+        """
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <title>Dataframe report</title>
+            <style type="text/css" media="screen">
+                h1 {
+                    background-color: #a8a8a8;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    text-align: center;
+                }
+
+                .styled-table {
+                    border-collapse: collapse;
+                    margin: 25px 0;
+                    font-size: 0.9em;
+                    font-family: sans-serif;
+                    min-width: 400px;
+                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                }
+
+                .styled-table thead tr {
+                    background-color: #009879;
+                    color: #ffffff;
+                    text-align: left;
+                }
+
+                .styled-table th,
+                .styled-table td {
+                    padding: 12px 15px;
+                }
+
+                .styled-table tbody tr {
+                    border-bottom: thin solid #dddddd;
+                }
+
+                .styled-table tbody tr:nth-of-type(even) {
+                    background-color: #f3f3f3;
+                }
+
+                .styled-table tbody tr.active-row {
+                    font-weight: bold;
+                    color: #009879;
+                }
+
+                .styled-table tbody tr:last-of-type {
+                    border-bottom: 2px solid #009879;
+                }
+            </style>
+        </head>
+        <body>"""
+        + html_table
+        + "</body>"
+    )
+
+    return pretty_html_table
