70676f929eec1198c2c674169726c43137adb4a1
Melvin Folkers <melvin@yellowstacks.nl>
2021-07-19 15:15:55 +0200

    test toegevoegd voor format numbers

--- notify/tests/test_email.py
+++ notify/tests/test_email.py
@@ -1,7 +1,9 @@
 import os
 import time
+
 from keyvault import secrets_to_environment
-from notify import NotifyMail
+
+from notify import NotifyMail, format_numbers
 from notify.tests import import_sample_dfs
 
 secrets_to_environment("notify")
@@ -9,9 +11,7 @@ secrets_to_environment("notify")
 
 def test_send_single_email():
     message = "This is a test from notify"
-    NotifyMail(
-        to=f"{os.environ.get('TEST_EMAIL_1')}", subject="Test Notify single email", message=message
-    ).send_email()
+    NotifyMail(to=f"{os.environ.get('TEST_EMAIL_1')}", subject="Test Notify single email", message=message).send_email()
     time.sleep(2)
 
 
@@ -22,6 +22,19 @@ def test_send_email_with_table():
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}",
         subject="Test Notify with HTML table",
+        message=message,
+        df=df,
+    ).send_email()
+    time.sleep(2)
+
+
+def test_send_email_with_formatted_table():
+    message = "This is a test from notify"
+    df = import_sample_dfs().get("Transactions")
+    df = format_numbers(df, currency_columns=["amount"])
+    NotifyMail(
+        to=f"{os.environ.get('TEST_EMAIL_1')}",
+        subject="Test Notify with HTML table with formatted columns",
         message=message,
         df=df,
     ).send_email()
@@ -52,4 +65,4 @@ def test_send_file():
 if __name__ == "__main__":
     # test_send_single_email()
     # test_send_multiple_emails()
-    test_send_email_with_table()
+    test_send_email_with_formatted_table()
