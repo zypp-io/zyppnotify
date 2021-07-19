42aeeb74f0ca4909c545a928da6dc2a4d909f067
Melvin Folkers <melvin@yellowstacks.nl>
2021-07-19 15:14:31 +0200

    format numbers toegevoegd aan init

--- notify/__init__.py
+++ notify/__init__.py
@@ -1,7 +1,8 @@
 import logging
 
-from notify.teams import NotifyTeams  # noqa
 from notify.mail import NotifyMail  # noqa
+from notify.teams import NotifyTeams  # noqa
+from notify.utils import format_numbers  # noqa
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(name)s] - %(message)s",
