33b570995d8f7fad9d108c5f7dfe12ce9933e465
Melvin Folkers <melvin@zypp.io>
2022-12-12 00:49:01 +0100

    adjusted email routine from smtp to graph api

--- notify/tests/test_email.py
+++ notify/tests/test_email.py
@@ -10,18 +10,20 @@ secrets_to_environment("notify")
 
 
 def test_send_single_email():
-    message = "This is a test from notify"
-    NotifyMail(to=f"{os.environ.get('TEST_EMAIL_1')}", subject="Test Notify single email", message=message).send_email()
+    message = "This is a e-mail test using the MS Graph API"
+    NotifyMail(
+        to=f"{os.environ.get('TEST_EMAIL_1')}", subject="Test Notify single email (Graph)", message=message
+    ).send_email()
     time.sleep(2)
 
 
 def test_send_email_with_table():
-    message = "This is a test from notify"
+    message = "This is a e-mail test using the MS Graph API"
     df = import_sample_dfs().get("Transactions")
 
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}",
-        subject="Test Notify with HTML table",
+        subject="Test Notify with HTML table (Graph)",
         message=message,
         df=df,
     ).send_email()
@@ -29,12 +31,12 @@ def test_send_email_with_table():
 
 
 def test_send_email_with_formatted_table():
-    message = "This is a test from notify"
+    message = "This is a e-mail test using the MS Graph API"
     df = import_sample_dfs().get("Transactions")
     df = format_numbers(df, currency_columns=["amount"])
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}",
-        subject="Test Notify with HTML table with formatted columns",
+        subject="Test Notify with HTML table with formatted columns (Graph)",
         message=message,
         df=df,
     ).send_email()
@@ -42,18 +44,18 @@ def test_send_email_with_formatted_table():
 
 
 def test_send_multiple_emails():
-    message = "This is a test from notify"
+    message = "This is a e-mail test using the MS Graph API"
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}, {os.environ.get('TEST_EMAIL_2')}",
-        subject="Test Notify multiple emails",
+        subject="Test Notify multiple emails (Graph)",
         message=message,
     ).send_email()
     time.sleep(2)
 
 
 def test_send_file():
-    message = "This is a test from notify"
-    subject = "Test Notify file"
+    message = "This is a e-mail test using the MS Graph API"
+    subject = "Test Notify file (Graph)"
     file_name = "2010 car efficiency.csv"
     file_path = os.path.join("notify", "tests", "data", file_name)
     NotifyMail(
@@ -65,8 +67,8 @@ def test_send_file():
 
 
 def test_send_file_from_storage():
-    message = "This is a test from notify"
-    subject = "Test Notify file Azure Storage"
+    message = "This is a e-mail test using the MS Graph API"
+    subject = "Test Notify file Azure Storage (Graph)"
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}",
         subject=subject,
@@ -75,12 +77,34 @@ def test_send_file_from_storage():
     ).send_email()
 
 
-def test_send_bcc():
-    message = "This is a test from notify"
+def test_send_cc():
+    message = "This is a e-mail test using the MS Graph API"
     NotifyMail(
         to=f"{os.environ.get('TEST_EMAIL_1')}",
-        bcc=f"{os.environ.get('TEST_EMAIL_2')}",
-        subject="Test Notify bcc emails",
+        cc=f"{os.environ.get('TEST_EMAIL_2')}",
+        subject="Test Notify cc emails (Graph)",
         message=message,
     ).send_email()
     time.sleep(2)
+
+
+def test_send_bcc():
+    message = "This is a e-mail test using the MS Graph API"
+    NotifyMail(
+        to=f"{os.environ.get('TEST_EMAIL_1')}",
+        bcc=f"{os.environ.get('TEST_EMAIL_2')}",
+        subject="Test Notify bcc emails (Graph)",
+        message=message,
+    ).send_email()
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+    test_send_single_email()
+    test_send_email_with_table()
+    test_send_file()
+    test_send_email_with_formatted_table()
+    test_send_multiple_emails()
+    test_send_file_from_storage()
+    test_send_cc()
+    test_send_bcc()
